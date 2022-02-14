848fa74b4eaf30371809cb89b12e78a69ce6c93a
johaa083 <Johaa083@gmail.com>
2022-02-14 11:41:36 +0100

    Php Changes

--- index.php
+++ index.php
@@ -25,14 +25,44 @@
       <?php 
 
 //Get access to the database
-require('database.php')
+require('database.php');
 
 //Insert values into database
-/*INSERT INTO booking (Name, Date, Gmail, Time)
-VALUES () ;*/
+if (isset($_REQUEST['name'])) {
+    // removes backslashes
+    $username = stripslashes($_REQUEST['name']);
+    //escapes special characters in a string
+    $username = mysqli_real_escape_string($con, $name);
+    $email    = stripslashes($_REQUEST['email']);
+    $email    = mysqli_real_escape_string($con, $email);
+    $create_datetime = date("Y-m-d H:i:s");
+    $query2   = "SELECT * FROM `booking` WHERE name='$name' OR email='" . ($email) . "' OR date";
+    $result = mysqli_query($con, $query2) or die;
+    function mysql_num_rows(){
+    }  
+    $rows = mysqli_num_rows($result);
+    
+    if ($rows == 0)
+    {
+        $query   = "INSERT into `users` (username, password, email, create_datetime)
+                    VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+        $insert = mysqli_query($con, $query);
+        if($insert) 
+        {
+            echo "<div class='form'>
+            <h3>You are registered successfully.</h3><br/>
+            <p class='link'>Click here to <a href='login.php'>Login</a></p>
+            </div>";
+        }
+        
+    }
+ 
+    
+}
 
 
 ?>
+
 
 </body>
 </html> 

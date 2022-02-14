023c79ba11c3c4d3176674318b71c672f793b4df
VILJAWEST <90932883+VILJAWEST@users.noreply.github.com>
2022-02-14 10:23:42 +0100

    css changes

--- index.php
+++ index.php
@@ -9,11 +9,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=".css">
     <title>Document</title>
 </head>
 <body>
 <h1>Welcome</h1>
 
+<a href="#opg1" class="card">
+        <div class="cardImg"></div>
+        <div class="cardText">
+            <h2>Oppdrag 1</h2>
+            <p>En mer brukervennlig Kuben nettside for Kuben</p>
+        </div>
+        <div class="cardcolor"></div>
+      </a>
     <?php
 
     ?>

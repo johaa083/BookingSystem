634f372861e35e6fa06d2202a4b4bc59c3775801
VILJAWEST <90932883+VILJAWEST@users.noreply.github.com>
2022-02-15 09:40:46 +0100

    fix

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,67 +7,65 @@
     <link rel="stylesheet" href="css.css">
     <title>BOOKING</title>
 </head>
-
 <body class="bodys1">
-    <h1>Welcome</h1>
-    -<div class="homeBackground"></div>
+<h1>Welcome</h1>
+-<div class="homeBackground"></div>
 
-    <button name="simple_button" type="button">
-        CUMSS
-    </button>
+<button class="button" name="simple_button" type="button">
+CUMSS
+</button>
 
-    <button name="submit_button" type="submit" value="Submit">
-        Submit
-    </button>
+<button name="submit_button" type="submit" value="Submit">
+    Submit
+</button>
 
-    <button name="reset_button" type="reset" value="Reset">
-        Reset
-    </button>
+<button name="reset_button" type="reset" value="Reset">
+    Reset
+</button>
 
 
-    <?php
-    //Get access to the database
-    require('database.php');
+<?php 
+//Get access to the database
+require('database.php');
 
-    //Insert values into database
-    if (isset($_REQUEST['name'])) {
-        $name = $_POST['name'];
-        $date = $_POST['date'];
-        $gmail = $_POST['gmail'];
+//Insert values into database
+if (isset($_REQUEST['name'])) {
+    $name = $_POST['name'];
+    $date = $_POST['date'];
+    $gmail = $_POST['gmail'];
 
-        //add to database
-        $sql = "INSERT INTO bookingsystem (name, date, gmail) 
+    //add to database
+    $sql = "INSERT INTO bookingsystem (name, date, gmail) 
             VALUES ('$name', '$date', $gmail)";
 
 
-        //database addition confirmation
-        if (mysqli_query($conn, $sql)) {
-            echo "Records inserted successfully.";
-        } else {
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-        }
-
-
-        // Close connection
-        $conn->close();
-
-        //}
-
-
-    ?>
-        <form class="form" action="" method="post">
-            <h1 class="login-title">Registration</h1>
-            <input type="text" class="login-input" name="username" placeholder="Username" required />
-            <input type="text" class="login-input" name="email" placeholder="Email Adress" required>
-            <input type="password" class="login-input" name="password" placeholder="Password" required>
-            <input type="submit" name="submit" value="Register" class="login-button">
-            <p class="link">Already have an account? <a href="login.php">Login here</a></p>
-        </form>
-    <?php
+    //database addition confirmation
+    if(mysqli_query($conn, $sql)){
+    echo "Records inserted successfully.";
+    }   
+    else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
     }
-    ?>
+
+
+// Close connection
+$conn->close();
+
+//}
+
+
+?>
+<h2>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="name" placeholder="Username" required />
+        <input type="submit" name="submit" value="Register" class="login-button">
+    </form>
+</h2>
+<?php
+}
+?>
 
 
 </body>
-
 </html>

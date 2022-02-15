f57e485d625f12c91a3e9e1f32b91f277007dfca
johaa083 <Johaa083@gmail.com>
2022-02-15 10:08:10 +0100

    fixing php

--- index.php
+++ index.php
@@ -97,14 +97,8 @@
     <center>
         <?php
   require('database.php');
-          
-        // Check connection
-        if($conn === false){
-            die("ERROR: Could not connect. " 
-                . mysqli_connect_error());
         }
-          
-        // Taking all 5 values from the form data(input)
+
         $name =  $_REQUEST['name'];
         $date = $_REQUEST['date'];
         $email = $_REQUEST['email'];
@@ -120,13 +114,7 @@
                 . " to view the updated data</h3>"; 
   
             echo nl2br("\n$name\n$date\n $email");
-        } else{
-            echo "ERROR: Hush! Sorry $sql. " 
-                . mysqli_error($conn);
-        }
-          
-        // Close connection
-        mysqli_close($conn);
+        } 
         ?>
     </center>
 

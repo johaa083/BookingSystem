5f7b5307da5b511a8a86e660df56eb663da28798
johaa083 <Johaa083@gmail.com>
2022-02-15 09:34:02 +0100

    my fixes

--- index.php
+++ index.php
@@ -68,11 +68,13 @@ $conn->close();
 
 
 ?>
-<form class="form" action="" method="post">
+<h2>
+    <form class="form" action="" method="post">
         <h1 class="login-title">Registration</h1>
         <input type="text" class="login-input" name="name" placeholder="Username" required />
         <input type="submit" name="submit" value="Register" class="login-button">
     </form>
+</h2>
 <?php
 }
 ?>

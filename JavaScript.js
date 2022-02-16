202fe74b3940225ff9a821a71fe92f04c4a0483a
VILJAWEST <90932883+VILJAWEST@users.noreply.github.com>
2022-02-16 09:26:50 +0100

    sus

--- JavaScript.js
+++ JavaScript.js
@@ -5,5 +5,5 @@ mybutton = document.getElementById("myBtn");
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollDown = 0; // For Safari
-    document.documentElement.scrollDown = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrolldown = 0; // For Chrome, Firefox, IE and Opera
 }

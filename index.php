2f06cd9b3f1863d27edb49752fae6b226970b071
johaa083 <Johaa083@gmail.com>
2022-02-16 10:10:12 +0100

    fix

--- index.php
+++ index.php
@@ -15,22 +15,22 @@
     <div class="homeBackground"></div>
 
     <section id="sec-1">
-    <dov class="container">
-    <h1>CUMS</h1>
-    <div class="scroll-down"></div>
-    </dov>
-   
+        <dov class="container">
+            <h1>CUMS</h1>
+            <div class="scroll-down"></div>
+        </dov>
 
 
-   </section>
 
-   <section class="sec-2">
-<div class="container">
-    <h1>this is sec 2</h1>
-</div>
+    </section>
 
-   </section>
- <center>
+    <section class="sec-2">
+        <div class="container">
+            <h1>this is sec 2</h1>
+        </div>
+
+    </section>
+    <center>
         <h1>Storing Form data in Database</h1>
 
         <form action="index.php" method="post">
@@ -50,7 +50,7 @@
                 <label for="emailAddress">Email Address:</label>
                 <input type="text" name="email" id="emailAddress" required>
             </p>
-            
+
 
             <input type="submit" value="Submit">
         </form>
@@ -64,42 +64,39 @@
             $name =  $_REQUEST['name'];
             $time = $_REQUEST['time'];
             $email = $_REQUEST['email'];
-        
-        $query2   = "SELECT * FROM `booking` WHERE name='$name' OR email='" . ($email) . "' OR time='$time'";
-        $result = mysqli_query($con, $query2) or die;
+
+            $query2   = "SELECT * FROM `booking` WHERE name='$name' OR email='" . ($email) . "' OR time='$time'";
+            $result = mysqli_query($con, $query2) or die;
 
 
 
-        
 
-        $rows = mysqli_num_rows($result);
 
-        if ($rows === 0) {
-            // Performing insert query execution
-            // here our table name is college
-            $sql = "INSERT INTO booking (name, time, email)
+            $rows = mysqli_num_rows($result);
+
+            if ($rows === 0) {
+                // Performing insert query execution
+                // here our table name is college
+                $sql = "INSERT INTO booking (name, time, email)
                     VALUES ('$name','$time','$email')";
 
-            $insert = mysqli_query($con, $sql);
-            if ($insert)
-            {
+                $insert = mysqli_query($con, $sql);
+                if ($insert) {
                     echo "<h3>data stored in a database successfully."
-                    . " Please browse your localhost php my admin"
-                    . " to view the updated data</h3>";
+                        . " Please browse your localhost php my admin"
+                        . " to view the updated data</h3>";
 
-                echo nl2br("\n$name\n$time\n$email");
-        } 
-    }
-        else {
-            echo "Sorry, there is already a reservation on that time. "
-                . mysqli_error($con);
+                    echo nl2br("\n$name\n$time\n$email");
+                }
+            } else {
+                echo "Sorry, there is already a reservation on that time. "
+                    . mysqli_error($con);
+            }
         }
-    
-    }
         // Close connection
         mysqli_close($con);
         ?>
-        
+
     </center>
 
     <script src="JavaScript.js"></script>

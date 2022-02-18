4b38d0035f9d3b3187df9381ec89959eee86014b
VILJAWEST <90932883+VILJAWEST@users.noreply.github.com>
2022-02-18 09:00:14 +0100

    Jonas er

--- JavaScript.js
+++ JavaScript.js
@@ -7,3 +7,7 @@ function topFunction() {
     document.body.scrollDown = 0; // For Safari
     document.documentElement.scrolldown = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
